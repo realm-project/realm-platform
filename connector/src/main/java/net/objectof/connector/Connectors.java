de3f5b7567e87a7bdd8d4227c6ed166fe1ea5d95
Nathaniel Sherry <nsherry4@uwo.ca>
2015-01-21 15:14:20 -0500

    Added exception handlers to Connectors, logging exceptions if one fails to load

--- connector/src/main/java/net/objectof/connector/Connectors.java
+++ connector/src/main/java/net/objectof/connector/Connectors.java
@@ -4,15 +4,28 @@ package net.objectof.connector;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 
 public class Connectors {
 
+    private static Log log = LogFactory.getLog(Connectors.class);
+
     public static List<Connector> getConnectors() {
         List<Connector> connectors = new ArrayList<>();
-
-        connectors.add(new ISQLiteConnector());
-        connectors.add(new IPostgresConnector());
-
+        try {
+            connectors.add(new ISQLiteConnector());
+        }
+        catch (Exception e) {
+            log.warn("Failed to load SQLite Connector", e);
+        }
+        try {
+            connectors.add(new IPostgresConnector());
+        }
+        catch (Exception e) {
+            log.warn("Failed to load Postgres Connector", e);
+        }
         return connectors;
     }
 
@@ -22,5 +35,4 @@ public class Connectors {
         }
         return null;
     }
-
 }
