31c5643874fdcc3052b976ae58e8c40ae1ea59d3
Nathaniel Sherry <nsherry4@uwo.ca>
2015-02-03 11:50:16 -0500

    Added IDeviceLatencyMonitorHandler

---
+++ corc/src/main/java/net/realmproject/platform/corc/accessor/IDeviceLatencyMonitorHandler.java
@@ -0,0 +1,43 @@
+package net.realmproject.platform.corc.accessor;
+
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+
+import net.objectof.corc.Action;
+import net.objectof.corc.web.v2.HttpRequest;
+import net.objectof.impl.corc.IHandler;
+import net.realmproject.dcm.accessor.DeviceLatencyMonitor;
+import net.realmproject.dcm.accessor.impl.IDeviceLatencyMonitor;
+import net.realmproject.dcm.event.bus.DeviceEventBus;
+import net.realmproject.platform.util.RealmSerialize;
+
+
+public class IDeviceLatencyMonitorHandler extends IHandler<HttpRequest> {
+
+    DeviceLatencyMonitor monitor;
+
+    public IDeviceLatencyMonitorHandler(String id, DeviceEventBus bus) {
+        super();
+        monitor = new IDeviceLatencyMonitor(id, bus);
+    }
+
+    @Override
+    public Class<? extends HttpRequest> getArgumentClass() {
+        return HttpRequest.class;
+    }
+
+    @Override
+    protected void onExecute(Action action, HttpRequest http) throws IOException, ServletException {
+        if (!"GET".equals(http.getHttpRequest().getMethod())) {
+            http.getHttpResponse().sendError(405); // Method Not Allowed
+            return;
+        }
+
+        Map<String, Long> map = Collections.singletonMap("latency", monitor.getLatency());
+        http.getWriter().write(RealmSerialize.serialize(map));
+    }
+}
--- util/src/main/java/net/realmproject/platform/util/RealmSerialize.java
+++ util/src/main/java/net/realmproject/platform/util/RealmSerialize.java
@@ -1,5 +1,6 @@
 package net.realmproject.platform.util;
 
+
 import java.io.Serializable;
 import java.util.Collections;
 import java.util.Map;
@@ -8,44 +9,45 @@ import flexjson.JSONDeserializer;
 import flexjson.JSONSerializer;
 import flexjson.transformer.AbstractTransformer;
 
+
 public class RealmSerialize {
-		
-	
-	public static Map<String, Serializable> structToMap(Object o) {
-		Object o2 = deserialize(serialize(o));
-		if (!(o2 instanceof Map)) o = Collections.singletonMap("value", o);
-		return (Map<String, Serializable>) deserialize(serialize(o));
-	}
 
-	public static <T> T convertMessage(Object publication, Class<T> clazz) {
-		String asString = serialize(publication);
-		return deserialize(asString, clazz);
-	}
+    public static Map<String, Serializable> structToMap(Object o) {
+        Object o2 = deserialize(serialize(o));
+        if (!(o2 instanceof Map)) o = Collections.singletonMap("value", o);
+        return (Map<String, Serializable>) deserialize(serialize(o));
+    }
 
-	public static Object deserialize(String json) {
-		return new JSONDeserializer<Object>().deserialize(json);
-	}
+    public static <T> T convertMessage(Object publication, Class<T> clazz) {
+        String asString = serialize(publication);
+        return deserialize(asString, clazz);
+    }
 
-	public static <T> T deserialize(String json, Class<T> clazz) {
-		return new JSONDeserializer<T>().deserialize(json, clazz);
-	}
+    public static Object deserialize(String json) {
+        return new JSONDeserializer<Object>().deserialize(json);
+    }
 
-	public static String serialize(Object o) {
-		return new JSONSerializer().transform(new ExcludeTransformer(), void.class).exclude("*.class").prettyPrint(true).deepSerialize(o);
-	}
+    public static <T> T deserialize(String json, Class<T> clazz) {
+        return new JSONDeserializer<T>().deserialize(json, clazz);
+    }
+
+    public static String serialize(Object o) {
+        return new JSONSerializer().transform(new ExcludeTransformer(), void.class).exclude("*.class")
+                .prettyPrint(true).deepSerialize(o);
+    }
 
 }
 
 class ExcludeTransformer extends AbstractTransformer {
 
-	@Override
-	public Boolean isInline() {
-		return true;
-	}
+    @Override
+    public Boolean isInline() {
+        return true;
+    }
 
-	@Override
-	public void transform(Object object) {
-		// Do nothing, null objects are not serialized.
-		return;
-	}
+    @Override
+    public void transform(Object object) {
+        // Do nothing, null objects are not serialized.
+        return;
+    }
 }
