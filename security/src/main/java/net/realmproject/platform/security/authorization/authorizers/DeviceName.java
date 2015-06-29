fbd42bbe1d4a1982eb4153156c49ee600bceb449
Nathaniel Sherry <nsherry4@uwo.ca>
2015-06-29 16:05:10 -0400

    Added 15-second grace period in device-name based authorization to reduce load on repo

--- security/src/main/java/net/realmproject/platform/security/authorization/authorizers/DeviceName.java
+++ security/src/main/java/net/realmproject/platform/security/authorization/authorizers/DeviceName.java
@@ -1,6 +1,10 @@
 package net.realmproject.platform.security.authorization.authorizers;
 
 
+import java.util.Date;
+
+import javax.servlet.http.HttpSession;
+
 import net.objectof.corc.Action;
 import net.objectof.corc.web.v2.HttpRequest;
 import net.realmproject.platform.schema.Device;
@@ -22,21 +26,37 @@ import net.realmproject.platform.util.model.Sessions;
  */
 public class DeviceName implements Authorizer {
 
+    private final String DEVICE_NAME_AUTHORIZATION_KEY = DeviceName.class.getCanonicalName() + ":";
+
     @Override
     public boolean authorize(Action action, HttpRequest request, Person person) {
 
+        HttpSession httpSession = request.getHttpRequest().getSession(false);
         String deviceName = RealmCorc.getNextPathElement(action.getName(), request.getHttpRequest());
+
+        // If this has been authorized any time in the last 15 seconds, don't
+        // bother re-authorizing
+        Long lastAuthorization = (Long) httpSession.getAttribute(authKey(deviceName));
+        if (lastAuthorization != null && lastAuthorization > new Date().getTime() - 15) { return true; }
 
         for (Session session : Persons.getActiveSessions(person)) {
             if (!Sessions.isLive(session)) continue;
 
             for (Device device : session.getStation().getDevices()) {
-                if (device.getName().equals(deviceName)) { return true; }
+                if (device.getName().equals(deviceName)) {
+                    httpSession.setAttribute(authKey(deviceName), new Date().getTime());
+                    return true;
+                }
             }
         }
 
+        httpSession.setAttribute(authKey(deviceName), null);
         return false;
 
+    }
+
+    private String authKey(String deviceName) {
+        return DEVICE_NAME_AUTHORIZATION_KEY + deviceName;
     }
 
 }
